c2e923ebb6cdd4ac0071c940047deccf9a631b49
captain listree <li@listree.com>
2017-01-02 13:33:30 -0800

    show how to aggregate meter data by user and by time

---
+++ src/main/java/meterprocess/MeterAggregatorByUser.java
@@ -0,0 +1,71 @@
+package meterprocess;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * mapreduce will aggregate meter usage by user
+ */
+public class MeterAggregatorByUser {
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable usage = new IntWritable(0);
+        private Text user = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            if (itr.hasMoreTokens()) {
+                itr.nextToken(); // ignore year
+                if (itr.hasMoreTokens()) {
+                    user.set(itr.nextToken());
+                    if (itr.hasMoreTokens()) {
+                        try {
+                            usage.set(new Integer(itr.nextToken()));
+                            context.write(user, usage);
+                        } catch(NumberFormatException nfe) {
+                            //ignore line
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "meter aggregator");
+        job.setJarByClass(MeterAggregatorByUser.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
